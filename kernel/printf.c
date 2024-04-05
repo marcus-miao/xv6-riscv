cb5f0f22e18683c931b44b24fbad7fe6fb0c87d5
marcus-miao <pmiao@andrew.cmu.edu>
2024-04-05 12:17:46 +0800

    implement backtrace && pass tests

--- kernel/printf.c
+++ kernel/printf.c
@@ -122,6 +122,7 @@ panic(char *s)
   printf(s);
   printf("\n");
   panicked = 1; // freeze uart output from other CPUs
+  backtrace();
   for(;;)
     ;
 }
@@ -131,4 +132,15 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+void
+backtrace()
+{
+  uint64 curr_fp = r_fp();
+  uint64 fp = curr_fp;
+  while (fp >= PGROUNDDOWN(curr_fp) && fp <= PGROUNDUP(curr_fp)) {
+    printf("%p\n", *(uint64 *)(fp - 8));
+    fp = *(uint64 *)(fp - 16);
+  }
 }
